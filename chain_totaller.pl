f6afcdc83bbc680ed368da20a9f374648d637c18
jeffdaly1975 <daly.jeff@gmail.com>
2025-06-23 11:52:12 -0400

    fixed bug where anything with 0.00 temporary total was assumed a MINEABLE

--- chain_totaller.pl
+++ chain_totaller.pl
@@ -25,8 +25,9 @@ foreach my $input_line (<>){
 
   if ($input_line =~ /^\s*\d+-chain/){
      chomp $input_line;
-     if ($input_line =~ /^\s*(\d+)-chain\s+(\w+-\d+)\s+::\s*0\.00 | MINEABLE/){
+     if ($input_line =~ /^\s*(\d+)-chain\s+(\w+-\d+)\s+::\s*0\.00 \| MINEABLE/){
        ($chains,$otom_name,$otom_mass) = ($1,$2,$3);
+#print STDERR "DEBUG: INPUT LINE IS MINEABLE: $input_line\n";
        $cumulative_total=0;
       #$input_otoms;
        $input_min_nrg=0;
@@ -42,20 +43,8 @@ foreach my $input_line (<>){
      }elsif ($input_line =~ /^\s*(\d+)-chain\s+(\w+)-(\d+)\s+::\s*([0-9.]+)\s\|(.*)\+\[\s*([0-9.]+)([* ])-\s*(\d+)\]\s=>\s*(.*?)\s*\|\s*(.*?)\s+\|\s*(OTOMRO.*)?$/){
        ($chains,$otom_name,$otom_mass,$cumulative_total,$input_otoms,$input_min_nrg,$minimal_indicator,$input_max_nrg,$output_otoms,$reaction_type,$otomro) = ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11);
 
-if ($otom_name ."-". $otom_mass =~ /Xj-16|K-29|C-53|Vt-98/){
- print STDERR "DEBUG: \$chains            [$chains]\n";
- print STDERR "DEBUG: \$otom_name         [$otom_name]\n";
- print STDERR "DEBUG: \$otom_mass         [$otom_mass]\n";
- print STDERR "DEBUG: \$cumulative_total  [$cumulative_total]\n";
- print STDERR "DEBUG: \$input_otoms       [$input_otoms]\n";
- print STDERR "DEBUG: \$input_min_nrg     [$input_min_nrg]\n";
- print STDERR "DEBUG: \$minimal_indicator [$minimal_indicator]\n";
- print STDERR "DEBUG: \$input_max_nrg     [$input_max_nrg]\n";
- print STDERR "DEBUG: \$output_otoms      [$output_otoms]\n";
- print STDERR "DEBUG: \$reaction_type     [$reaction_type]\n";
- print STDERR "DEBUG: \$otomro            [" . (defined $otomro ? $otomro : "<undefined>") ."]\n";
+#print STDERR "DEBUG: INPUT LINE IS CHAIN RECIPE: $input_line\n";
 
-}
        # zero the cumulative - we are recalculating these
        $cumulative_total = 0;
 
@@ -69,20 +58,12 @@ if ($otom_name ."-". $otom_mass =~ /Xj-16|K-29|C-53|Vt-98/){
 
              die "no preexisting minimal value for input otom [$in] for input_line[$input_line]\n" unless exists $db{$in};
           }
-if ($otom_name ."-". $otom_mass =~ /Xj-16|K-29|C-53|Vt-98/){
-  print STDERR "DEBUG: INPUT otom: [$in] adding [$db{$in}]\n";
-  print STDERR "DEBUG: CUMULATIVE TOTAL NOW: $cumulative_total\n";
-}
+
           $cumulative_total += $db{$in};
        }
 
        # the total cost for this is all input otoms cost plus input NRG
        $cumulative_total += $input_min_nrg;
-
-if ($otom_name ."-". $otom_mass =~ /Xj-16|K-29|C-53|Vt-98/){
-  print STDERR "DEBUG: INPUT NRG: [$input_min_nrg]\n";
-  print STDERR "DEBUG: CUMULATIVE TOTAL NOW: $cumulative_total\n";
-}
 
        # each of the otoms in the output will get this calculated recipe cost.
        # but first check that it doesn't already exist from a lower costing recipe that might have been on the previous line.
@@ -93,7 +74,7 @@ if ($otom_name ."-". $otom_mass =~ /Xj-16|K-29|C-53|Vt-98/){
           if (exists $db{$out}){
                if ($db{$out} <= $cumulative_total){
                  # thats cheaper so do not change it
-#                warn "DEBUG: not updating db[$out] since [$db{$out}] < $cumulative_total  for input_line[$input_line]\n";
+                 warn "DEBUG: not updating db[$out] since [$db{$out}] < $cumulative_total  for input_line[$input_line]\n";
                }else{
                  print STDERR "DEBUG: updating db[$out] since [$db{$out}] > $cumulative_total  for input_line[$input_line]\n";
                  print STDERR "DEBUG: NOTE THIS PROBABLY IS A CHEAPER RECIPE CHAIN!\n";
@@ -118,6 +99,7 @@ if ($otom_name ."-". $otom_mass =~ /Xj-16|K-29|C-53|Vt-98/){
 
   }elsif($input_line =~ /^\s*[+|]|^\s*$/){
     # documentation line or whitespace
+#print STDERR "DEBUG: INPUT LINE IS DOCUMENTATION OR WHITESPACE:\n";
     print $input_line;
   }else{
     warn "invalid input line: $input_line\n";
